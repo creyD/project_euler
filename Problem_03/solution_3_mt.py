f4bc6f3367aa2814e6a4284096fa60101d315b1f
Conrad Großer <grosserconrad@gmail.com>
2018-10-25 00:10:18 +0200

    Currected Multithreading Import

--- Problem_03/solution_3_mt.py
+++ Problem_03/solution_3_mt.py
@@ -2,7 +2,7 @@
 # Source: https://projecteuler.net/problem=3
 
 import time
-import thread
+import _thread
 start_time = time.time()
 
 def isPrime(number):
@@ -13,7 +13,7 @@ def isPrime(number):
 			return False
 	return True
 
-def thread(rangeBegin, rangeEnd, starting_number):
+def calcThread(rangeBegin, rangeEnd, starting_number):
 	print('Thread started (' + str(rangeBegin) + '-' + str(rangeEnd) + ')')
 	for i in range(rangeBegin, rangeEnd):
 		if (starting_number % i == 0) and isPrime(i):
@@ -25,14 +25,14 @@ largestFactor, starting_number = 0, 600851475143
 goal = round(starting_number / 2)
 cache = []
 
-thread.start_new_thread(cache.append(thread(2, goal/8, starting_number)))
-thread.start_new_thread(cache.append(thread(goal/8 + 1, goal/4 , starting_number)))
-thread.start_new_thread(cache.append(thread(goal/4 + 1, 3 * goal/8, starting_number)))
-thread.start_new_thread(cache.append(thread(3 * goal/8 + 1, goal/2, starting_number)))
-thread.start_new_thread(cache.append(thread(goal/2 + 1, 5 * goal/8, starting_number)))
-thread.start_new_thread(cache.append(thread(5 * goal/8 + 1, 3 * goal/4, starting_number)))
-thread.start_new_thread(cache.append(thread(3 * goal/4 + 1, 7 * goal/8, starting_number)))
-thread.start_new_thread(cache.append(thread(7 * goal/8 + 1, goal, starting_number)))
+_thread.start_new_thread(cache.append(calcThread(2, round(goal/8), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(round(goal/8) + 1, round(goal/4), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(round(goal/4) + 1, 3 * round(goal/8), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(3 * round(goal/8) + 1, round(goal/2), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(round(goal/2) + 1, 5 * round(goal/8), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(5 * round(goal/8) + 1, 3 * round(goal/4), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(3 * round(goal/4) + 1, 7 * round(goal/8), starting_number)))
+_thread.start_new_thread(cache.append(calcThread(7 * round(goal/8) + 1, goal, starting_number)))
 
 print(max(cache))
 
