bcc73867513b6434cd9e83c63f31410caec53d51
Conrad Großer <grosserconrad@gmail.com>
2018-10-24 19:50:08 +0200

    Added Solution 3 in Python

---
+++ Problem_03/solution_3.py
@@ -0,0 +1,26 @@
+# Project Euler Problem 3
+# Source: https://projecteuler.net/problem=3
+
+import time
+start_time = time.time()
+
+def isPrime(number):
+	if number % 2 == 0 and number > 2:
+		return False
+	for i in range(3, int(number ** 0.5) + 1, 2):
+		if number % i == 0:
+			return False
+	return True
+
+largestFactor, starting_number = 0, 600851475143
+
+for i in range(2, round(starting_number/2)):
+	if isPrime(i) and (starting_number % i == 0) and i > largestFactor:
+		largestFactor = i
+
+print(i)
+
+print("The script took %s seconds for execution" % (time.time() - start_time))
+
+# Solution: 6857 (Correct)
+# Runtime: 
