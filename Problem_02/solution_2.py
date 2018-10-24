c57888da3f10a554f60c7f46d5c4b1620f4872fe
Conrad Großer <grosserconrad@gmail.com>
2018-10-24 18:26:28 +0200

    Improved Solution 2 in Python

--- Problem_02/solution_2.py
+++ Problem_02/solution_2.py
@@ -4,18 +4,19 @@
 import time
 start_time = time.time()
 
-fibonacciNumbers, summe = [1, 2], 2
+all_fibo, summe = [1, 2], 2
 
 while True:
-	bla = fibonacciNumbers[-1] + fibonacciNumbers[-2]
-	if bla >= 4000000:
+	next_number = all_fibo[-1] + all_fibo[-2]
+	if next_number >= 4000000:
 		break
-	elif bla % 2 == 0:
-		summe += bla
-	fibonacciNumbers.append(bla)
-		
+	elif next_number % 2 == 0:
+		summe += next_number
+	all_fibo[0] = all_fibo[1]
+	all_fibo[1] = next_number
+
 print(summe)
 print("The script took %s seconds for execution" % (time.time() - start_time))
 
-# Solution: 233168 (still correct)
-# Runtime: 0.00025200843811 seconds
+# Solution: 4613732 (Correct)
+# Runtime: 6.103515625e-05 seconds
